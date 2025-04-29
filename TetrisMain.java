b5c1ce188efb8ac92c526ef13af176283ae6bd6e
Niqolas Gonzalez <gonzal257@rowan.edu>
2025-04-29 13:10:16 -0400

    added music

--- TetrisMain.java
+++ TetrisMain.java
@@ -9,5 +9,11 @@ public class TetrisMain {
         frame.setResizable(false);
         frame.setContentPane(new TetrisGamePanel());
         frame.setVisible(true);
+
+        audioPlayer theme = new audioPlayer("Tetris.wav");
+        Thread thread1 = new Thread(theme);
+        thread1.start();
+
+
     }
 }
